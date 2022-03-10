0169bafcf9964d7673ac70550f7fc26bac18916e
Timofey Vorobiev <vorobiev.te@yandex.ru>
2022-03-10 14:05:51 +0300

    1st

--- homework.py
+++ homework.py
@@ -50,7 +50,7 @@ def send_message(bot, message):
         logger.info(f'Отправка сообщения: {message}')
 
 
-def get_api_answer(ENDPOINT, current_timestamp):
+def get_api_answer(current_timestamp):
     """Отправляет запрос к API."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
